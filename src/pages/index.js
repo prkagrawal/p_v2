b4506907b85a97fa17da972847f20a3d03f95ade
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-11-10 11:12:16 +0530

    initial cleanup and configurations

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <h1>Welcome</h1>
+    </div>
+  )
+}
