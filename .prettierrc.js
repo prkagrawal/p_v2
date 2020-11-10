71bb4e204b6f4c5a90e5eb57974c946dcc747033
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-11-10 11:54:22 +0530

    setup prettier, lint-staged and husky

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  parser: "babel",
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+};
