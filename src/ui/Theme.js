185c2150f920d15226e170dc6d9ad64ece669c19
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-11-13 22:44:49 +0530

    setup theme-ui

--- src/ui/Theme.js
+++ src/ui/Theme.js
@@ -91,3 +91,19 @@ export const DarkTheme = {
   primaryText: "#F8F8F8",
   ...CommonTheme,
 };
+
+export default {
+  colors: {
+    text: "#111",
+    background: "#fff",
+    primary: "tomato",
+    secondary: "#3f3f3f",
+    muted: "#e0e0e0",
+    highlight: "#9f9f9f",
+    gray: "#6c6c6c",
+    accent: "#3f3f3f",
+  },
+  fonts: {
+    body: "Comic Sans MS",
+  },
+};
